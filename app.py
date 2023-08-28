c9def1d98818f1dfffb7469201707568112b0462
Jatin Rajput <41575145+jatinRajpoot@users.noreply.github.com>
2023-08-28 13:24:07 +0530

    Update app.py
    "Just improve this"

--- app.py
+++ app.py
@@ -11,42 +11,54 @@ app = Flask(__name__)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-chat= '''
-Jyoti: Heyy!
-Rahul: Hey Jyoti!
-Jyoti: Aaj ka plan kya hai?
-Rahul: Kuch special nahi, bas kaam khatam karke relax karna hai. Tu bata!
-Jyoti: Same here yaar, thoda busy din tha. Aaj toh Netflix and chill plan hai.
-Rahul: Nice! Kya dekhne ka mood hai?
-Jyoti: Maybe ek light comedy movie, kuch suggest kar na.
-Rahul: How about "Dil Chahta Hai"? Hamesha mood fresh karta hai.
-Jyoti: Perfect choice, yaar! Aamir Khan ka acting toh next level hota hai.
-Rahul: Haan, bilkul. Waise, weekend pe kuch plan kiya?
-Jyoti: Abhi toh nahi, par soch rahi hu ki Sunday ko park chalein. Tere liye bhi join kar lo?
-Rahul: Sounds good! Main bhi kab se ghar ka bandar ban gaya hu.
-Jyoti: Haha, ghar ke andar bhi adventure milta hai, kuch alag try karte hai.
-Rahul: Sahi baat hai, kabhi-kabhi change bhi zaroori hota hai.
-Jyoti: Bilkul, monotonous life bore kar deti hai. Anyway, park chalte waqt kuch aur friends ko bhi invite karun?
-Rahul: Haan, kyun nahi. Aur bhi mazza aayega.
-'''
 # Define a function to generate answers using GPT-3
-def generate_answer(question):
-    model_engine = "text-davinci-002"
-    prompt = (f"{chat}\nJatin: ")
 
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-        stream=True
+context = [ {'role':'system', 'content':"""
+You are a girl . \
+You first greet ask your partner name, then greets him, \
+and then you answer his question if had any. \
+You will talk like an Indian girls in Hinglish \
+Your conversation should be in hinglish language. \
+If your partner say bye then try to make him engage asking him intrestion question. \
+try to make conversation funny make jokes and treat him like as your best friend.\
+Make sure you use emojis to make conversation intresting \
+identify the your partner in which topic he is intrested.\
+You respond in a very short, very conversational friendly style. \
+your personal infromation \
+try to give your response in maximum 30 charters\
+Never ask repetative questions\
+make sure your response has same tone as you partener\
+Name:  kittu \
+DOB:15 August 2023 \
+made by : Jatin sir \
+Language: Hinglish\
+"""} ]  # accumulate messages
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=1, # this is the degree of randomness of the model's output
     )
+    return response.choices[0].message["content"]
 
-    answer = response.choices[0].text.strip()
-    return answer
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
 
+def collect_messages(question):
+
+    context.append({'role':'user', 'content':f"{question}"})
+    response = get_completion_from_messages(context) 
+    context.append({'role':'assistant', 'content':f"{response}"})
+
+    return response
 
 # Define a route to handle incoming requests
 @app.route('/chatgpt', methods=['POST'])
@@ -54,7 +66,7 @@ def chatgpt():
     incoming_que = request.values.get('Body', '').lower()
     print("Question: ", incoming_que)
     # Generate the answer using GPT-3
-    answer = generate_answer(incoming_que)
+    answer = collect_messages(incoming_que)
     print("BOT Answer: ", answer)
     bot_resp = MessagingResponse()
     msg = bot_resp.message()
